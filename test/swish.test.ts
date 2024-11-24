ae29f0c17caba9d45a0246326ca5240059e26e3b
alexisveryreal <alexmaple20@gmail.com>
2024-11-24 15:05:39 -0800

    feat: added more tests for standard request methods
    * also removed the main.ts file since it was just for testing

--- test/swish.test.ts
+++ test/swish.test.ts
@@ -3,13 +3,16 @@ import { swish } from "../src/index.ts";
 import { Elysia } from "elysia";
 
 describe("Swish", () => {
-  test("logs correct messages", async () => {
+  test("logs correct messages GET", async () => {
     // Mock `console.log` to capture its calls
     const logMock = spyOn(console, "log").mockImplementation(() => {});
 
     const app = new Elysia()
       .use(swish({ level: "verbose", gargles: { colors: false } }))
-      .get("/", () => "hi");
+      .get("/", async () => {
+        await Bun.sleep(10);
+        return "hi";
+      });
 
     const response = await app
       .handle(new Request("http://localhost/"))
@@ -19,6 +22,75 @@ describe("Swish", () => {
 
     const secondLog = logMock.mock.calls[1][0];
     expect(secondLog).toMatch(/^-> GET \/ 200 \d+\.\d{2}$/);
+
+    // restore console.log
+    logMock.mockRestore();
+  });
+
+  test("logs correct messages POST", async () => {
+    const logMock = spyOn(console, "log").mockImplementation(() => {});
+
+    const app = new Elysia()
+      .use(swish({ level: "verbose", gargles: { colors: false } }))
+      .post("/", async () => {
+        await Bun.sleep(10);
+        return "hi";
+      });
+
+    const response = await app
+      .handle(new Request("http://localhost/", { method: "POST" }))
+      .then((res) => res.text());
+
+    expect(logMock.mock.calls[0]).toEqual(["<- POST /"]);
+
+    const secondLog = logMock.mock.calls[1][0];
+    expect(secondLog).toMatch(/^-> POST \/ 200 \d+\.\d{2}$/);
+
+    // restore console.log
+    logMock.mockRestore();
+  });
+
+  test("logs correct messages DELETE", async () => {
+    const logMock = spyOn(console, "log").mockImplementation(() => {});
+
+    const app = new Elysia()
+      .use(swish({ level: "verbose", gargles: { colors: false } }))
+      .delete("/", async () => {
+        await Bun.sleep(10);
+        return "hi";
+      });
+
+    const response = await app
+      .handle(new Request("http://localhost/", { method: "DELETE" }))
+      .then((res) => res.text());
+
+    expect(logMock.mock.calls[0]).toEqual(["<- DELETE /"]);
+
+    const secondLog = logMock.mock.calls[1][0];
+    expect(secondLog).toMatch(/^-> DELETE \/ 200 \d+\.\d{2}$/);
+
+    // restore console.log
+    logMock.mockRestore();
+  });
+
+  test("logs correct messages PUT", async () => {
+    const logMock = spyOn(console, "log").mockImplementation(() => {});
+
+    const app = new Elysia()
+      .use(swish({ level: "verbose", gargles: { colors: false } }))
+      .put("/", async () => {
+        await Bun.sleep(10);
+        return "hi";
+      });
+
+    const response = await app
+      .handle(new Request("http://localhost/", { method: "PUT" }))
+      .then((res) => res.text());
+
+    expect(logMock.mock.calls[0]).toEqual(["<- PUT /"]);
+
+    const secondLog = logMock.mock.calls[1][0];
+    expect(secondLog).toMatch(/^-> PUT \/ 200 \d+\.\d{2}$/);
 
     // restore console.log
     logMock.mockRestore();
